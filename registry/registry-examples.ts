9b3505a8d2c0064aef2d9a28c1a231f67b4266a0
Isaac Hermel <isaachermel@gmail.com>
2025-04-17 10:08:12 -0300

    fix: switch examples to blocks in registry

--- registry/registry-examples.ts
+++ registry/registry-examples.ts
@@ -3,98 +3,98 @@ import { Registry } from 'shadcn/registry';
 export const examples: Registry['items'] = [
   {
     name: 'use-window-size-demo',
-    type: 'registry:example',
+    type: 'registry:block',
     title: 'UseWindowSizeDemo',
     description: "use-window-size's hook in action.",
     registryDependencies: ['https://h3-use.vercel.app/r/use-window-size'],
     files: [
       {
         path: 'registry/examples/use-window-size-demo.tsx',
-        type: 'registry:example',
+        type: 'registry:block',
         target: 'components/examples/use-window-size-demo.tsx',
       },
     ],
   },
   {
     name: 'use-media-demo',
-    type: 'registry:example',
+    type: 'registry:block',
     title: 'UseMediaDemo',
     description: "use-media's hook in action.",
     registryDependencies: ['https://h3-use.vercel.app/r/use-media'],
     files: [
       {
         path: 'registry/examples/use-media-demo.tsx',
-        type: 'registry:example',
+        type: 'registry:block',
         target: 'components/examples/use-media-demo.tsx',
       },
     ],
   },
   {
     name: 'use-mouse-demo',
-    type: 'registry:example',
+    type: 'registry:block',
     title: 'UseMouseDemo',
     description: "use-mouse's hook in action.",
     registryDependencies: ['https://h3-use.vercel.app/r/use-mouse'],
     files: [
       {
         path: 'registry/examples/use-mouse-demo.tsx',
-        type: 'registry:example',
+        type: 'registry:block',
         target: 'components/examples/use-mouse-demo.tsx',
       },
     ],
   },
   {
     name: 'use-keypress-demo',
-    type: 'registry:example',
+    type: 'registry:block',
     title: 'UseKeypressDemo',
     description: "use-keypress's hook in action.",
     registryDependencies: ['https://h3-use.vercel.app/r/use-keypress'],
     files: [
       {
         path: 'registry/examples/use-keypress-demo.tsx',
-        type: 'registry:example',
+        type: 'registry:block',
         target: 'components/examples/use-keypress-demo.tsx',
       },
     ],
   },
   {
     name: 'use-orientation-demo',
-    type: 'registry:example',
+    type: 'registry:block',
     title: 'UseOrientationDemo',
     description: "use-orientation's hook in action.",
     registryDependencies: ['https://h3-use.vercel.app/r/use-orientation'],
     files: [
       {
         path: 'registry/examples/use-orientation-demo.tsx',
-        type: 'registry:example',
+        type: 'registry:block',
         target: 'components/examples/use-orientation-demo.tsx',
       },
     ],
   },
   {
     name: 'use-toggle-demo',
-    type: 'registry:example',
+    type: 'registry:block',
     title: 'UseToggleDemo',
     description: "use-toggle's hook in action.",
     registryDependencies: ['https://h3-use.vercel.app/r/use-toggle'],
     files: [
       {
         path: 'registry/examples/use-toggle-demo.tsx',
-        type: 'registry:example',
+        type: 'registry:block',
         target: 'components/examples/use-toggle-demo.tsx',
       },
     ],
   },
   {
     name: 'use-set-demo',
-    type: 'registry:example',
+    type: 'registry:block',
     title: 'UseSetDemo',
     description: "use-set's hook in action.",
     registryDependencies: ['https://h3-use.vercel.app/r/use-set'],
     files: [
       {
         path: 'registry/examples/use-set-demo.tsx',
-        type: 'registry:example',
+        type: 'registry:block',
         target: 'components/examples/use-set-demo.tsx',
       },
     ],
