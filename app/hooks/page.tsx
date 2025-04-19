81fcc032c5558c339bc7d3035f5b7e1a37ac812b
Isaac Hermel <isaachermel@gmail.com>
2025-04-19 18:58:01 -0300

    feat: add basic metadata to /hooks

--- app/hooks/page.tsx
+++ app/hooks/page.tsx
@@ -1,8 +1,16 @@
+import { Metadata } from 'next';
+
 import { CategoryCard } from '@/components/category-card';
 import { PageBanner } from '@/components/page-banner';
 import { PageGrid } from '@/components/page-grid';
 
 import { categories } from '@/config/hooks';
+
+export const metadata: Metadata = {
+  title: 'Hooks',
+  description:
+    'A collection of reusable and customizable hooks built with and for React.',
+};
 
 export default function HooksPage() {
   const sortedCategories = categories.sort((a, b) => {
