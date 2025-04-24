e9ac0c49632169ef15f32de7c3e582cbed876bfc
Isaac Hermel <isaachermel@gmail.com>
2025-04-23 22:20:05 -0300

    feat(hooks): update use-update-effect dmeo

--- registry/example/use-update-effect-demo.tsx
+++ registry/example/use-update-effect-demo.tsx
@@ -3,7 +3,13 @@
 import React, { useState } from 'react';
 
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 import { useUpdateEffect } from '@/registry/hooks/use-update-effect';
 
@@ -19,6 +25,10 @@ export default function UseUpdateEffectDemo() {
     <Card className="relative max-w-md w-full">
       <CardHeader>
         <CardTitle>useUpdateEffect</CardTitle>
+        <CardDescription>
+          This example uses the <code>useUpdateEffect</code> hook to execute a
+          function when the count state is updated.
+        </CardDescription>
       </CardHeader>
       <CardContent className="space-y-4">
         <div className="flex flex-col items-center gap-4">
