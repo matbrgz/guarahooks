e547a382681979d3f6f419cb53f027559838be95
Isaac Hermel <isaachermel@gmail.com>
2025-04-23 22:23:53 -0300

    feat(hooks): update use-on-unmount

--- registry/hooks/use-on-unmount.tsx
+++ registry/hooks/use-on-unmount.tsx
@@ -1,11 +1,15 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export function useOnUnmount(callback: () => void): void {
+  const fnRef = useRef(callback);
+
+  fnRef.current = callback;
+
   useEffect(() => {
     return () => {
-      callback();
+      fnRef.current();
     };
   }, []);
 }
